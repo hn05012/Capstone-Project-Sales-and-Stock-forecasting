cae8cce5a139f06ade59052edf5b96ee66fe3aa1
hasan <hasannaseem2@gmail.com>
2022-04-24 21:31:04 +0500

    junk files removed from developer branch

--- Codes/convert.py
+++
@@ -1,9 +0,0 @@
-import tabula
-
-
-df = tabula.read_pdf("Lahore Branch Data (E4)/PDF Reports/Current Stock/CurrStock 5 dec 2021.pdf")
-tabula.convert_into(
-    "Lahore Branch Data (E4)/PDF Reports/Current Stock/CurrStock 5 dec 2021.pdf", "Code/Curr_Stock/CS1.csv", pages='all')
-
-
-
